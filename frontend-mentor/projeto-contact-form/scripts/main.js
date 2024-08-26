d104e2e2cf0b708594c62be2c131a91e5d13995e
Douglas Silva <douglas302004@gmail.com>
2024-08-26 18:15:40 -0300

    Finalização do projeto - Validação de formulário de contato

---
+++ frontend-mentor/projeto-contact-form/scripts/main.js
@@ -0,0 +1,86 @@
+const form = document.querySelector('form');
+const fields = document.querySelectorAll('input');
+const message = document.querySelector('textarea');
+const btnSubmit = document.querySelector('#btn-submit');
+const error = document.querySelectorAll('.error');
+
+const modal = document.querySelector('.modal');
+const btnModal = document.querySelector('#btn-modal');
+
+const addOrRemoveError = (index, adiciona) => {
+   if (adiciona === true) {
+      error[index].classList.add('active');
+   } else {
+      error[index].classList.remove('active');
+   }
+};
+
+const emailIsValid = email => {
+   let isValid = /\S+@\S+\.\S+/;
+   return isValid.test(email);
+};
+
+btnSubmit.addEventListener('click', event => {
+   event.preventDefault();
+
+   let isValid = true;
+
+   for (let i = 0; i < 2; i++) {
+      if (fields[i].value === '') {
+         addOrRemoveError(i, true);
+         isValid = false;
+      } else {
+         addOrRemoveError(i, false);
+      }
+   }
+
+   if (fields[2].value === '') {
+      addOrRemoveError(2, true);
+      addOrRemoveError(3, true);
+      isValid = false;
+   } else if (!emailIsValid(fields[2].value)) {
+      addOrRemoveError(2, false);
+      addOrRemoveError(3, true);
+      isValid = false;
+      console.log(isValid);
+   } else {
+      addOrRemoveError(2, false);
+      addOrRemoveError(3, false);
+   }
+
+   if (fields[3].checked || fields[4].checked) {
+      addOrRemoveError(4, false);
+   } else {
+      addOrRemoveError(4, true);
+      isValid = false;
+   }
+
+   if (message.value === '') {
+      addOrRemoveError(5, true);
+      isValid = false;
+   } else {
+      addOrRemoveError(5, false)
+   }
+
+   if (!fields[5].checked) {
+      addOrRemoveError(6, true);
+      isValid = false;
+   } else {
+      addOrRemoveError(6, false);
+   }
+
+   if (isValid === true) {
+      modal.classList.add('active');
+
+      btnModal.addEventListener('click', () => {
+         fields.forEach((field, index) => {
+            field.value = '';
+            field.checked = false;
+            message.value = '';
+            modal.classList.remove('active');
+         })
+
+         fields[6].value = "Submit";
+      })
+   }
+});
